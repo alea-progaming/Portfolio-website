a7a42809a9683aad49ed2ad30a8a9b6f1aeb914d
Mikaela Purugganan <mikaela.programming@gmail.com>
2024-08-13 06:23:27 +0800

    typo

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navabr = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('#header nav a');
+let menuIcon = document.querySelector("#menu-icon");
+let navabr = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("#header nav a");
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttriute('id');
+    let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navlinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
-      })
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a [href*=" + id + " ]")
+          .classList.add("active");
+      });
     }
-  })
-}
+  });
+};
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navabr.classList.toggle('active');
-}
+  menuIcon.classList.toggle("bx-x");
+  navabr.classList.toggle("active");
+};
